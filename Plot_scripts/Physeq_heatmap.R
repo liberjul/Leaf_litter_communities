d8b2445f3669ecb794f903df316132d238652150
liberjul <liberjul@msu.edu>
2020-04-14 15:08:33 -0400

    Added weights vector, labelling of facets.

--- Plot_scripts/Physeq_heatmap.R
+++ Plot_scripts/Physeq_heatmap.R
@@ -3,6 +3,7 @@ library(vegan)
 library(phyloseq)
 
 setwd("C:/Users/julia/OneDrive - Michigan State University/Documents/MSU/Undergrad/Fall 2018/PLP 847/miseq_dat/Leaf_litter_communities")
+wts_wo_negs <- c(rep(18,18), rep(10,10), rep(8, 8), rep(10, 10)) # Vector for weighting abundance by number of samples per substrate
 map_wo_negs <- as.matrix(read.csv("./Data/DEM_map_wo_negs.csv", stringsAsFactors = F))
 rare_otu <- as.matrix(read.csv("./Data/Rare_otu_table.csv"))
 
@@ -26,7 +27,12 @@ phy_sample_table <- sample_data(data.frame(Substrate = map_wo_negs["Substrate",]
 
 physeq1 <- merge_phyloseq(physeq, phy_sample_table) # Merge sample data to physeq object
 ps_heatmap <- plot_heatmap(physeq1, "NMDS", "bray", "Sample") # Make heatmap
-ps_heatmap <- ps_heatmap + facet_grid(~Substrate, switch = "x", scales = "free_x", space = "free_x") + # Add facets for Substrate
+facet_labs <- c(Epi = "Epiphyte", Endo = "Endophyte", Lit = "Litter", Soil = "Soil") # Change facet labels
+ps_heatmap <- ps_heatmap + facet_grid(~Substrate,
+                                      switch = "x",
+                                      scales = "free_x",
+                                      space = "free_x",
+                                      labeller = labeller(Substrate=facet_labs)) + # Add facets for Substrate
   theme(axis.text.x.bottom = element_text(angle=90, vjust=0.5, hjust=1)) + # Adjust the column labels
   labs(x = "Sample Name", fill = "Proportion\n of Substrate") # Change x label
 ps_heatmap
