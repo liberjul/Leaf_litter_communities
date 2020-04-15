418746e6b1cfc93df8c14f00c01a6fb672d362de
liberjul <liberjul@msu.edu>
2020-04-15 10:37:19 -0400

    Added varriability metric justification for BC dissimilarity.

--- Plot_scripts/NMDS_all_samples_plots.R
+++ Plot_scripts/NMDS_all_samples_plots.R
@@ -52,12 +52,14 @@ p_bc <- ggplot(data = NMDS_bray, aes(MDS1, MDS2)) + # Make plot
                          guide_legend(label.theme = element_text(size = 10, angle = 0, face = "italic"))) +
   theme_pubr() +
   guides(fill=FALSE) +
-  ggtitle("Bray-Curtis") +
+  # ggtitle("Bray-Curtis") +
   theme(plot.title = element_text(hjust=0.5),
         legend.position = "right",
         legend.justification = "left") +
   scale_color_discrete(labels=c("Endophytes","Epiphytes","Litter", "Soil"))
 p_bc
+
+ggsave("./Figures/bc_NDMS_all_samples.png", p_bc, width = 8, height = 6, units="in")
 
 MDS_dat <- metaMDS(t(rare_otu), distance = "jaccard") # Calculate NMDS axes, using Jaccard distance
 MDS_points <- MDS_dat$points # Extract coordinates
@@ -76,22 +78,78 @@ for(g in levels(MDS_dat_df$Soil_Leaf_Litter_Leaf_swab)){
                                 ,group=g))
 }
 NMDS_jac.mean=aggregate(NMDS_jac[,1:2],list(group=NMDS_jac$group),mean) # Calculate mean for groups
-p_jac <- ggplot(data = NMDS_jac, aes(MDS1, MDS2)) + # Make plot
-  geom_point(aes(color = group, shape = site),size=3) +
-  geom_point(aes(color = group, fill = group, alpha = species, shape=site),size=3) +
-  geom_path(data=df_ell_jac, aes(x=NMDS1, y=NMDS2,color=group), size=1, linetype=2) +
-  labs(alpha="Host species", color="Substrate", shape="Site") +
+# p_jac <- ggplot(data = NMDS_jac, aes(MDS1, MDS2)) + # Make plot
+#   geom_point(aes(color = group, shape = site),size=3) +
+#   geom_point(aes(color = group, fill = group, alpha = species, shape=site),size=3) +
+#   geom_path(data=df_ell_jac, aes(x=NMDS1, y=NMDS2,color=group), size=1, linetype=2) +
+#   labs(alpha="Host species", color="Substrate", shape="Site") +
+#   scale_shape_manual(values = 21:25) +
+#   scale_alpha_manual(values=c(0,1), guide =
+#                        guide_legend(label.theme = element_text(size = 10, angle = 0, face = "italic"))) +
+#   guides(fill=FALSE) +
+#   ggtitle("Jaccard") +
+#   theme_pubr() +
+#   theme(plot.title = element_text(hjust=0.5),
+#         legend.position = "right",
+#         legend.justification = "left") +
+#   scale_color_discrete(labels=c("Endophytes","Epiphytes","Litter", "Soil"))
+# p_jac
+
+# combined_NMDS <- p_bc + p_jac + plot_layout(guides="collect")
+# ggsave("./Figures/Combined_NMDS_w_site.png", combined_NMDS, height=6, width=8, units="in")
+
+bc.d = vegdist(t(rare_otu), method="bray")
+sor.d = vegdist(t(rare_otu), method = "bray", binary = T)
+jac.d = vegdist(t(rare_otu), method="jaccard")
+
+bc.pcoa = cmdscale(bc.d, eig=T)
+ax1.v.bc = bc.pcoa$eig[1]/sum(bc.pcoa$eig)
+ax2.v.bc = bc.pcoa$eig[2]/sum(bc.pcoa$eig)
+
+sor.pcoa = cmdscale(sor.d, eig=T)
+ax1.v.sor = sor.pcoa$eig[1]/sum(sor.pcoa$eig)
+ax2.v.sor = sor.pcoa$eig[2]/sum(sor.pcoa$eig)
+
+jac.pcoa = cmdscale(jac.d, eig=T)
+ax1.v.jac = jac.pcoa$eig[1]/sum(jac.pcoa$eig)
+ax2.v.jac = jac.pcoa$eig[2]/sum(jac.pcoa$eig)
+
+ax1.v.bc
+ax1.v.sor
+ax1.v.jac
+
+ax2.v.bc
+ax2.v.sor
+ax2.v.jac
+
+ax1.v.bc + ax2.v.bc
+ax1.v.sor + ax2.v.sor
+ax1.v.jac + ax2.v.jac
+
+bc.pcoa_df <- data.frame(ax1 = bc.pcoa$points[,1],
+                         ax2 = bc.pcoa$points[,2],
+                         substrate=MDS_dat_df$Substrate,
+                         species=MDS_dat_df$Plant_species,
+                         site=MDS_dat_df$Site)
+
+pcoa_bc <- ggplot(data = bc.pcoa_df, aes(ax1, ax2)) + # Make plot
+  geom_point(aes(color = substrate, shape = site),size=3) +
+  geom_point(aes(color = substrate, fill = substrate, alpha = species, shape=site),size=3) +
+  labs(alpha="Host species", color="Substrate", shape="Site",
+       x = paste("PCoA1: ",100*round(ax1.v.bc,3),"% var. explained",sep=""),
+       y = paste("PCoA2: ",100*round(ax2.v.bc,3),"%var. explained",sep="")) +
   scale_shape_manual(values = 21:25) +
   scale_alpha_manual(values=c(0,1), guide =
                        guide_legend(label.theme = element_text(size = 10, angle = 0, face = "italic"))) +
   guides(fill=FALSE) +
-  ggtitle("Jaccard") +
   theme_pubr() +
   theme(plot.title = element_text(hjust=0.5),
         legend.position = "right",
         legend.justification = "left") +
   scale_color_discrete(labels=c("Endophytes","Epiphytes","Litter", "Soil"))
-p_jac
+pcoa_bc
+ggsave("./Figures/bc_pcoa_all_samples.png", pcoa_bc, width = 8, height=6, units="in")
 
-combined_NMDS <- p_bc + p_jac + plot_layout(guides="collect")
-ggsave("./Figures/Combined_NMDS_w_site.png", combined_NMDS, height=6, width=8, units="in")
+ado_bray_pcoa <- adonis2(t(rare_otu) ~ substrate * species * site,
+                    bc.pcoa_df, method="bray")
+ado_bray_pcoa
